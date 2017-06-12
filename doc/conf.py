6981b0b5ec20278e7c77dfdf0eff5a1a5ba167e9
Benoit Bovy <benbovy@gmail.com>
2017-06-12 14:20:53 +0200

    try fix readthedocs take 5

--- doc/conf.py
+++ doc/conf.py
@@ -51,7 +51,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.extlinks',
     'sphinx.ext.napoleon',
-    #'numpydoc',
+    # 'numpydoc',
     'IPython.sphinxext.ipython_directive',
     'IPython.sphinxext.ipython_console_highlighting',
 ]
@@ -64,8 +64,8 @@ extlinks = {
 autosummary_generate = True
 
 napoleon_numpy_docstring = True
-#numpydoc_class_members_toctree = True
-#numpydoc_show_class_members = False
+# numpydoc_class_members_toctree = True
+# numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -125,7 +125,7 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # otherwise, readthedocs.org uses their theme by default, so no need
 # to specify it
