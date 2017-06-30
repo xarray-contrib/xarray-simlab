46ed13a7baa23b3a89b838eb636d73c0d159895e
Benoit Bovy <benbovy@gmail.com>
2017-06-30 17:52:42 +0200

    wip tests  (model)

--- xsimlab/tests/test_model.py
+++ xsimlab/tests/test_model.py
@@ -1,9 +1,10 @@
 import unittest
 
 import numpy as np
+from numpy.testing import assert_array_equal
 
-from xsimlab.variable.base import (Variable, ForeignVariable, VariableList,
-                                   VariableGroup, diagnostic)
+from xsimlab.variable.base import (Variable, ForeignVariable, VariableGroup,
+                                   diagnostic)
 from xsimlab.process import Process
 from xsimlab.model import Model
 
@@ -41,7 +42,7 @@ class SomeProcess(Process):
     some_effect = Variable('x', group='effect', provided=True)
 
     def run_step(self, dt):
-        self.some_effect.value = self.x * self.some_param.value + dt
+        self.some_effect.value = self.x.value * self.some_param.value + dt
 
 
 class OtherProcess(Process):
@@ -51,7 +52,7 @@ class OtherProcess(Process):
     other_effect = Variable('x', group='effect', provided=True)
 
     def run_step(self, dt):
-        self.other_effect.value = self.x * self.other_param.value - dt
+        self.other_effect.value = self.x.value * self.other_param.value - dt
 
     @diagnostic
     def x2(self):
@@ -59,7 +60,7 @@ class OtherProcess(Process):
 
 
 class MetaProcess(Process):
-    param = Variable(())
+    meta_param = Variable(())
     some_param = ForeignVariable(SomeProcess, 'some_param')
     other_param = ForeignVariable(OtherProcess, 'other_param')
 
@@ -69,6 +70,12 @@ class MetaProcess(Process):
 
 
 class TestModel(unittest.TestCase):
+
+    def setUp(self):
+        self.model = Model({'grid': Grid,
+                            'some_process': SomeProcess,
+                            'other_process': OtherProcess,
+                            'quantity': Quantity})
 
     def test_constructor(self):
         # test invalid processes
@@ -84,15 +91,44 @@ class TestModel(unittest.TestCase):
         with self.assertRaisesRegex(TypeError, "is not a subclass"):
             Model({'invalid_class': OtherClass})
 
-        model = Model({'grid': Grid, 'some_process': SomeProcess,
-                       'other_process': OtherProcess, 'quantity': Quantity})
-
         # test process ordering
-        sorted_process_names = list(model.keys())
+        sorted_process_names = list(self.model.keys())
         self.assertEqual(sorted_process_names[0], 'grid')
         self.assertEqual(sorted_process_names[-1], 'quantity')
         self.assertIn('some_process', sorted_process_names[1:-1])
         self.assertIn('other_process', sorted_process_names[1:-1])
 
         # test dict-like vs. attribute access
-        self.assertIs(model['grid'], model.grid)
+        self.assertIs(self.model['grid'], self.model.grid)
+
+    def test_input_vars(self):
+        expected = {'grid': ['x_size'],
+                    'some_process': ['some_param'],
+                    'other_process': ['other_param'],
+                    'quantity': ['quantity']}
+        actual = {k: list(v.keys()) for k, v in self.model.input_vars.items()}
+        self.assertDictEqual(expected, actual)
+
+    def test_is_input(self):
+        self.assertTrue(self.model.is_input(self.model.grid.x_size))
+        self.assertTrue(self.model.is_input(('grid', 'x_size')))
+        self.assertFalse(self.model.is_input(('quantity', 'all_effects')))
+
+    def test_initialize(self):
+        model = self.model.clone()
+        model.grid.x_size.value = 10
+        model.initialize()
+        expected = np.arange(10)
+        assert_array_equal(model.grid.x.value, expected)
+
+    def test_run_step(self):
+        model = self.model.clone()
+        model.grid.x_size.value = 10
+        model.some_process.some_param.value = 1
+        model.other_process.other_param.value = 1
+
+        model.initialize()
+        model.run_step(100)
+
+        expected = model.grid.x.value * 2
+        assert_array_equal(model.quantity.quantity.change, expected)
