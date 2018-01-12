07c403b5d75e915070a8c15a5f7edef51a8aa5e6
Benoit Bovy <benbovy@gmail.com>
2018-01-12 17:10:44 +0100

    some updates in MANIFEST.in and setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 from os.path import exists
 
 import versioneer
@@ -15,10 +15,11 @@ setup(name='xarray-simlab',
       maintainer='Benoit Bovy',
       maintainer_email='benbovy@gmail.com',
       license='BSD-Clause3',
-      keywords='python xarray modelling simulation-framework',
-      packages=['xsimlab', 'xsimlab.variable'],
+      keywords='python xarray modelling simulation framework',
+      packages=find_packages(),
       long_description=(open('README.rst').read() if exists('README.rst')
                         else ''),
       python_requires='>=3.4',
       install_requires=['numpy', 'xarray >= 0.8.0'],
+      tests_require=['pytest >= 3.3.0'],
       zip_safe=False)
