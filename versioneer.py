a088d1146c8ede39a47e9b680a8c4eb4ce8ed476
Raphael Lange <rlange@sec55-dynip-222.gfz-potsdam.de>
2020-01-08 14:28:06 +0100

    Updated versioneer and _version

--- versioneer.py
+++ versioneer.py
@@ -1015,7 +1015,7 @@ def git_versions_from_keywords(keywords, tag_prefix, verbose):
         # "stabilization", as well as "HEAD" and "master".
         tags = set([r for r in refs if re.search(r"\d", r)])
         if verbose:
-            print("discarding '%s', no digits" % ",".join(refs - tags))
+            print(f"discarding '{','.join(refs - tags)}', no digits")
     if verbose:
         print(f"likely tags: {','.join(sorted(tags))}")
     for ref in sorted(tags):
