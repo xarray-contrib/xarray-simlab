76286a9b9922c1d383056e2ae72be49d261eb928
Benoit Bovy <benbovy@gmail.com>
2020-02-28 12:37:24 +0100

    wip add tests

--- xsimlab/tests/test_stores.py
+++ xsimlab/tests/test_stores.py
@@ -1,7 +1,15 @@
+from tempfile import mkdtemp
+
 import numpy as np
 from numpy.testing import assert_array_equal
+import pytest
+import xarray as xr
+import zarr
 
-from xsimlab.stores import _get_output_steps_by_clock
+from xsimlab.stores import (
+    _get_output_steps_by_clock,
+    ZarrOutputStore
+)
 
 
 def test_get_output_steps_by_clock(in_dataset):
@@ -16,3 +24,25 @@ def test_get_output_steps_by_clock(in_dataset):
     assert actual.keys() == expected.keys()
     for k in expected:
         assert_array_equal(actual[k], expected[k])
+
+
+class TestZarrOutputStore:
+
+    @pytest.mark.parametrize(
+        "zobject", [None, mkdtemp(), zarr.MemoryStore(), zarr.group()]
+    )
+    def test_constructor(self, in_dataset, model, zobject):
+        out_store = ZarrOutputStore(in_dataset, model, zobject)
+
+        assert out_store.zgroup.store is not None
+
+    def test_write_input_xr_dataset(self, in_dataset, model):
+        out_store = ZarrOutputStore(in_dataset, model, None)
+
+        out_store.write_input_xr_dataset()
+        ds = xr.open_zarr(out_store.zgroup.store, chunks=None)
+
+        xr.testing.assert_equal(ds, in_dataset)
+
+        # check output variables attrs removed before saving input dataset
+        assert not ds.xsimlab.output_vars
