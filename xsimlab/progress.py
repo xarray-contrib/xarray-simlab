1ca0813c5d969ba35cba2d128f82a2c6125f6573
Raphael Lange <rlange@sec55-dynip-223.gfz-potsdam.de>
2020-03-02 14:29:29 +0100

    variable names

--- xsimlab/progress.py
+++ xsimlab/progress.py
@@ -5,10 +5,9 @@ from tqdm.auto import tqdm
 
 class ProgressBar(RuntimeHook):
     """
-    Progress bar from tqdm package.
-    For additional customization, consult: https://tqdm.github.io/docs/tqdm/
+    Progress bar implementation using the tqdm package.
+    For additional customization, see: https://tqdm.github.io/docs/tqdm/
     """
-
     def __init__(self, **kwargs):
         self.pbar_dict = {}
         if kwargs:
@@ -16,14 +15,14 @@ class ProgressBar(RuntimeHook):
 
     @runtime_hook("initialize", trigger="pre")
     def init_bar(self, model, context, state):
-        self.pbar_dict.update(total=context["step_total"].values)
-        self.pb_model = tqdm(**self.pbar_dict)
+        self.pbar_dict.update(total=context['step_total'].values)
+        self.pbar_model = tqdm(**self.pbar_dict)
 
     @runtime_hook("run_step", "model", trigger="post")
     def update_bar(self, mode, context, state):
-        self.pb_model.update(1)
+        self.pbar_model.update(1)
 
     @runtime_hook("finalize", trigger="post")
     def close_bar(self, model, context, state):
-        self.pb_model.update(1)
-        self.pb_model.close()
+        self.pbar_model.update(1)
+        self.pbar_model.close()
