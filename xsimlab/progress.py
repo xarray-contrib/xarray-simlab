c70570d9b02cfc8d9f7c2f8ef48bec5f2d073c18
Raphael Lange <rlange@macbook-pro-2.localdomain>
2020-03-06 21:29:40 +0100

    added more tests

--- xsimlab/progress.py
+++ xsimlab/progress.py
@@ -1,7 +1,7 @@
-from xsimlab.hook import RuntimeHook, runtime_hook
-
 import tqdm
 from tqdm.auto import tqdm as auto
+
+from xsimlab.hook import RuntimeHook, runtime_hook
 
 
 class ProgressBar(RuntimeHook):
@@ -35,22 +35,29 @@ class ProgressBar(RuntimeHook):
     For additional customization, see: https://tqdm.github.io/docs/tqdm/
     """
 
+    env_list = ["auto", "console", "gui", "notebook"]
+
     def __init__(self, frontend="auto", **kwargs):
         self.frontend = frontend
-        self.pbar_dict = {"bar_format": "{desc}{bar}{percentage:3.1f}%"}
+        self.pbar_dict = {"bar_format": "{desc} {bar} {percentage:3.1f}%"}
         self.pbar_dict.update(kwargs)
 
     @runtime_hook("initialize", trigger="pre")
     def init_bar(self, model, context, state):
         self.pbar_dict.update(total=context["nsteps"] + 2, desc="initialize")
-        if self.frontend == "notebook":
-            self.pbar_model = tqdm.tqdm_notebook(**self.pbar_dict)
-        elif self.frontend == "console":
-            self.pbar_model = tqdm.tqdm(**self.pbar_dict)
-        elif self.frontend == "auto":
+
+        if self.frontend not in self.env_list:
+            raise ValueError(
+                f"Frontend argument {self.frontend!r} not supported. Please select one of the following: {', '.join(self.env_list)}"
+            )
+        elif self.frontend == self.env_list[0]:
             self.pbar_model = auto(**self.pbar_dict)
-        elif self.frontend == "gui":
+        elif self.frontend == self.env_list[1]:
+            self.pbar_model = tqdm.tqdm(**self.pbar_dict)
+        elif self.frontend == self.env_list[2]:
             self.pbar_model = tqdm.tqdm_gui(**self.pbar_dict)
+        elif self.frontend == self.env_list[3]:
+            self.pbar_model = tqdm.tqdm_notebook(**self.pbar_dict)
 
     @runtime_hook("initialize", trigger="post")
     def update_init(self, mode, context, state):
--- xsimlab/tests/test_progress.py
+++ xsimlab/tests/test_progress.py
@@ -2,6 +2,7 @@ import pytest
 import tqdm
 
 import xsimlab as xs
+from .fixture_model import in_dataset, model
 
 
 def test_tqdm():
@@ -16,7 +17,22 @@ def test_tqdm():
 
 
 def test_bar():
-    pbar = xs.progress.ProgressBar()
+    pbar_default = xs.progress.ProgressBar()
+    pbar_auto = xs.progress.ProgressBar(frontend="auto")
+    pbar_console = xs.progress.ProgressBar(frontend="console")
+    pbar_gui = xs.progress.ProgressBar(frontend="gui")
+    pbar_notebook = xs.progress.ProgressBar(frontend="notebook")
 
-    assert pbar.frontend == "auto"
-    assert "{bar}" in pbar.pbar_dict["bar_format"]
+    assert pbar_auto.frontend in pbar_default.env_list
+    assert pbar_console.frontend in pbar_default.env_list
+    assert pbar_gui.frontend in pbar_default.env_list
+    assert pbar_notebook.frontend in pbar_default.env_list
+    assert "{bar}" in pbar_default.pbar_dict["bar_format"]
+
+
+def test_environment_error(in_dataset, model):
+    with pytest.raises(ValueError) as env:
+        pbar = xs.progress.ProgressBar(frontend="ipython")
+        out_ds = in_dataset.xsimlab.run(model=model, hooks=[pbar])
+
+    assert "not supported" in str(env.value)
