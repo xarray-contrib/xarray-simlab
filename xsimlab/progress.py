97408bcd3bd213bcf9787d2b5cbb178789263e2a
rlange2 <raphael.lange@mail.de>
2020-03-10 22:04:29 +0100

    rework import

--- xsimlab/progress.py
+++ xsimlab/progress.py
@@ -1,6 +1,3 @@
-import tqdm
-from tqdm.auto import tqdm as auto
-
 from xsimlab.hook import RuntimeHook, runtime_hook
 
 
@@ -38,40 +35,44 @@ class ProgressBar(RuntimeHook):
     env_list = ["auto", "console", "gui", "notebook"]
 
     def __init__(self, frontend="auto", **kwargs):
-        self.frontend = frontend
-        self.pbar_dict = {"bar_format": "{desc} {bar} {percentage:3.1f}%"}
+        if frontend == "auto":
+            from tqdm.auto import tqdm
+        elif frontend == "console":
+            from tqdm import tqdm
+        elif frontend == "gui":
+            from tqdm.gui import tqdm
+        elif frontend == "notebook":
+            from tqdm.notebook import tqdm
+        else:
+            raise ValueError(
+                f"Frontend argument {self.frontend!r} not supported. Please select one of the following: {', '.join(self.env_list)}"
+            )
+
+        self.tqdm = tqdm
+        self.pbar_dict = {"bar_format": "{bar} {desc} {percentage:3.0f}%"}
         self.pbar_dict.update(kwargs)
 
     @runtime_hook("initialize", trigger="pre")
     def init_bar(self, model, context, state):
         self.pbar_dict.update(total=context["nsteps"] + 2, desc="initialize")
-
-        if self.frontend not in self.env_list:
-            raise ValueError(
-                f"Frontend argument {self.frontend!r} not supported. Please select one of the following: {', '.join(self.env_list)}"
-            )
-        elif self.frontend == self.env_list[0]:
-            self.pbar_model = auto(**self.pbar_dict)
-        elif self.frontend == self.env_list[1]:
-            self.pbar_model = tqdm.tqdm(**self.pbar_dict)
-        elif self.frontend == self.env_list[2]:
-            self.pbar_model = tqdm.tqdm_gui(**self.pbar_dict)
-        elif self.frontend == self.env_list[3]:
-            self.pbar_model = tqdm.tqdm_notebook(**self.pbar_dict)
+        self.pbar_model = self.tqdm(**self.pbar_dict)
 
     @runtime_hook("initialize", trigger="post")
     def update_init(self, mode, context, state):
         self.pbar_model.update(1)
 
     @runtime_hook("run_step", trigger="post")
-    def update_bar(self, mode, context, state):
+    def update_runstep(self, mode, context, state):
         self.pbar_model.set_description_str(
             f"run step {context['step']}/{context['nsteps']}"
         )
         self.pbar_model.update(1)
 
+    @runtime_hook("finalize", trigger="pre")
+    def update_finalize(self, model, context, state):
+        self.pbar_model.set_description_str("finalize")
+
     @runtime_hook("finalize", trigger="post")
     def close_bar(self, model, context, state):
-        self.pbar_model.set_description_str("finalize")
         self.pbar_model.update(1)
         self.pbar_model.close()
