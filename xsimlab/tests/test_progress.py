faa9647580572104665de8bf0a0d15f9d8704849
rlange2 <raphael.lange@mail.de>
2020-03-10 20:17:08 +0100

    update test section Test names have been specified, 3rd party test logic removed, tests are skipped if not run on python3.7, tqdm dependency has been added to pass travis, minor modifications.

--- xsimlab/tests/test_progress.py
+++ xsimlab/tests/test_progress.py
@@ -1,38 +1,20 @@
 import pytest
+import sys
 import tqdm
 
 import xsimlab as xs
 from .fixture_model import in_dataset, model
 
 
-def test_tqdm():
-    pbar = tqdm.tqdm(range(5))
-
-    assert pbar.desc == ""
-    assert pbar.total == 5
-
-    pbar.set_description_str("update")
-
-    assert pbar.desc == "update"
+@pytest.mark.skipif(sys.version_info != (3, 7), reason="test performed in python3.7")
+@pytest.mark.parametrize("test_input", ["auto", "console", "gui", "notebook"])
+def test_progress_bar_init(test_input):
+    pbar = xs.progress.ProgressBar()
+    assert test_input in pbar.env_list
 
 
-def test_bar():
-    pbar_default = xs.progress.ProgressBar()
-    pbar_auto = xs.progress.ProgressBar(frontend="auto")
-    pbar_console = xs.progress.ProgressBar(frontend="console")
-    pbar_gui = xs.progress.ProgressBar(frontend="gui")
-    pbar_notebook = xs.progress.ProgressBar(frontend="notebook")
-
-    assert pbar_auto.frontend in pbar_default.env_list
-    assert pbar_console.frontend in pbar_default.env_list
-    assert pbar_gui.frontend in pbar_default.env_list
-    assert pbar_notebook.frontend in pbar_default.env_list
-    assert "{bar}" in pbar_default.pbar_dict["bar_format"]
-
-
-def test_environment_error(in_dataset, model):
-    with pytest.raises(ValueError) as env:
-        pbar = xs.progress.ProgressBar(frontend="ipython")
+@pytest.mark.skipif(sys.version_info != (3, 7))
+def test_progress_bar_init_error(in_dataset, model):
+    with pytest.raises(ValueError, match=r".*not supported.*"):
+        pbar = xs.progress.ProgressBar(frontend="invalid_frontend")
         out_ds = in_dataset.xsimlab.run(model=model, hooks=[pbar])
-
-    assert "not supported" in str(env.value)
