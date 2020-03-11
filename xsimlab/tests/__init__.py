48ec96e4331918193b84e3a43343fe7cddf2820b
rlange2 <raphael.lange@mail.de>
2020-03-11 10:41:34 +0100

    black

--- xsimlab/tests/__init__.py
+++ xsimlab/tests/__init__.py
@@ -2,13 +2,10 @@ import importlib
 import pytest
 
 
-def _importorskip(modname, minversion=None):
+def _importorskip(modname):
     try:
         mod = importlib.import_module(modname)
         has = True
-        if minversion is not None:
-            if LooseVersion(mod.__version__) < LooseVersion(minversion):
-                raise ImportError("Minimum version not satisfied")
     except ImportError:
         has = False
     func = pytest.mark.skipif(not has, reason=f"requires {modname}")
