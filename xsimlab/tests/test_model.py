803f65244d9167e766f681ba550438dcdec4275f
Joeperdefloep <joepgevaert@gmail.com>
2021-04-06 10:04:24 +0200

    improved test #178

--- xsimlab/tests/test_model.py
+++ xsimlab/tests/test_model.py
@@ -498,16 +498,12 @@ class TestModel:
         class D:
             pass
 
-        @xs.process
-        class E:
-            pass
-
         model = xs.Model(
-            {"a": A, "b": B, "c": C, "d": D, "e": E},
-            custom_dependencies={"d": "c", "c": "b", "b": {"a", "e"}},
+            {"a": A, "b": B, "c": C, "d": D},
+            custom_dependencies={"d": "c", "c": "b", "b": "a"},
         )
         model = model.drop_processes(["b", "c"])
-        assert set(model.dependent_processes["d"]) == {"a", "e"}
+        assert model.dependent_processes["d"] == ["a"]
 
     def test_visualize(self, model):
         pytest.importorskip("graphviz")
